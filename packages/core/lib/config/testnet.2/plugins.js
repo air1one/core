63e8561ba60332c8dfe4bc1732669620cfb9b01d
Air1 <erwann@ark.io>
2018-09-13 12:48:37 +0400

    reset to 0e73f2c3df3e23dbd23c336a4efc496a8ff42240

--- packages/core/lib/config/testnet.2/plugins.js
+++ packages/core/lib/config/testnet.2/plugins.js
@@ -24,14 +24,15 @@ module.exports = {
   '@arkecosystem/core-database': {
     snapshots: `${process.env.ARK_PATH_DATA}/${process.env.ARK_NETWORK_NAME}.2/snapshots`
   },
-  '@arkecosystem/core-database-postgres': {
-    connection: {
-      host: process.env.ARK_DB_HOST || 'localhost',
-      port: process.env.ARK_DB_PORT || 5432,
-      database: process.env.ARK_DB_DATABASE || `ark_${process.env.ARK_NETWORK_NAME}2`,
-      user: process.env.ARK_DB_USERNAME || 'ark',
-      password: process.env.ARK_DB_PASSWORD || 'password'
-    },
+  '@arkecosystem/core-database-sequelize': {
+    dialect: 'sqlite',
+    storage: process.env.ARK_DB_STORAGE || `${process.env.ARK_PATH_DATA}/database/${process.env.ARK_NETWORK_NAME}.2.sqlite`,
+    // host: process.env.ARK_DB_HOST || 'localhost',
+    // dialect: process.env.ARK_DB_DIALECT || 'postgres',
+    // username: process.env.ARK_DB_USERNAME || 'ark',
+    // password: process.env.ARK_DB_PASSWORD || 'password',
+    // database: process.env.ARK_DB_DATABASE || 'ark_testnet2',
+    logging: process.env.ARK_DB_LOGGING,
     redis: {
       host: process.env.ARK_REDIS_HOST || 'localhost',
       port: process.env.ARK_REDIS_PORT || 6379
