7be0fd5bdfd30503af5b7faeda31152c2f6e229a
Air1 <erwann@ark.io>
2018-09-29 13:19:14 +0400

    chore: record requests

--- packages/core/lib/start-relay-and-forger.js
+++ packages/core/lib/start-relay-and-forger.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const nock = require('nock')
 const container = require('@arkecosystem/core-container')
 
 /**
@@ -8,6 +9,8 @@ const container = require('@arkecosystem/core-container')
  * @return {void}
  */
 module.exports = async (options) => {
+  nock.recorder.rec()
+
   await container.setUp(options, {
     options: {
       '@arkecosystem/core-p2p': {
